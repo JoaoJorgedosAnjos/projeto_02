49c589f47960dd56e242d09a00fa388c002ad0f8
joaojorge <joaojorgedosanjos@gmail.com>
2024-09-08 23:59:24 -0300

    :recycle: refactor: correção dos texto para criação do dicionário

--- assets/conto_nosso_povo_cap_1_ptbr_ticuna.py
+++ assets/conto_nosso_povo_cap_1_ptbr_ticuna.py
@@ -1,9 +1,54 @@
-portugues = '''
-00 Antes do mundo existir,
-01 Ngutapa já existia.
-02 Ele não teve pai nem mãe.'''
-
-ticuna = '''
-00 Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü,
-01 rü nge'eǖ rü na tauma i ta'acü i nhũma na yema i na i aẽǖrü'ǖ.
-02 Rü yiĩ'ǖ ngema na yamareǖ ga Ngutapa.'''
+portugues = """
+01 NOSSO POVO
+02 Aqui começa a história do tempo dos antigos
+03 O início da história
+04 Antes do mundo existir, Ngutapa já existia.
+05 se criou junto com
+06 Baia era parente de Ngutapa
+07 É no igarapé Tonetü.
+08 Naquele tempo, a terra ainda estava se formando. O mato era baixinho e o rio ainda tinha pouca água, lá eles viviam.
+09 Passaram-se muitos anos.
+10 Nunca tiveram filhos.
+11 quando o mato já estava crescido,
+12 Ngutapa foi caçar com Mapana.
+13 Depois disso, amarrou Mapana num pau, de braços e pernas abertos.
+14 Vieram as cabas e as formigas e morderam a sua periquita.
+16 Vovó, pode me desamarrar?
+17 Vovó, venha me desamarrar.
+18 Aquele desgraçado me prendeu aqui para me matar - ela falou de novo.
+19 chegando mais perto, perguntou: - O que lhe aconteceu, minha neta?
+20 O cancã se transformou em gente
+21 Se você quiser se vingar de Ngutapa, 
+22 está aqui a caba. 
+23 Ela pegou a caba e guardou. 
+24 Depois disso, o cancã se transformou em pássaro e foi embora. 
+25 Vinha tocando flauta e pulava numa perna e noutra, cantando:
+26 Mapana estava, esperando.
+27 Escondida num tronco de árvore... Desde que as cabas ferram, seus joelhos começaram a inchar."""
+ticuna = """
+01 TORÜ DUǕ'ǕGÜ
+02 Nori arü ügü ga ore ga nucüma'ǖ ga torü ga na nhunhaacü yi'i'ǖ.
+03 Norü ügü tchiga
+04 Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü,
+05 rü wü'iva na yae
+06 erü nüma ga Baia rü Ngutapa tanü'ǖ ni'ĩ
+07 natü i Tonetü
+08 Erü yeguma ãrü naãenecü rü wü'i i ngewaca i ya eaneǖrǖ'ǖ ni'ĩ yerü natauma ga nhãtü rü yema nawa nayemagüǖ ricatama ni'ĩ.
+09 marü nhurema ya taunecǖ na yemagü
+10 rü tauguma naãcü
+11 yerü yeguma rü marü nayaanegü ga naĩnecü
+12 Rü yeguma fenewa tümamãã na u'ǖ ga namá ga Ngutapa
+13 rü mu'utchicüma ya tauenecü na ngeacü.
+14 rü maẽ rü tü'ǖ nawi rü ẽne, rü tü'ü na ngo ga tümaãrü ngaǖwa.
+16 Pa noẽ pa coou tautchiname'ǖ ega tchoǖ icuwẽgǖgu
+17 pa noẽ tauchina meǖ ega tchoǖ icuiwẽgügu
+18 rü tchoǖ ni'ĩ ma, i ucaǖǖtchi i Ngutapa nhãtagürü.
+19  Rü yeguma ga noẽ ga Coou ta ngica itarü ĩ rü ngiǖ ta ngaǖ rü cu tücü pa tchauta’ã nhãtagürü.
+20 Rü yeguma ga noẽ ga coocou rü ĩrü duü
+21 rü ngeguma cuütanütchaǖgu
+22 daani'ĩ ya maẽ nhanagürü
+23  yeguma rü inayau ga maẽ
+24 Rü marü yemawena ga coou rü tüna i ĩgü rü weriǖ ti'igüãrü
+25 Rü nüma ga Ngutapa rü ni'ĩ woweruetchigü rü ni'ĩ wü'i paratchigü, rü ni'ĩ fenagütchigü
+26 ngima ga Mapana rü marü iya nguẽẽ
+27 ga namacüwawa ga naĩpünewa... Rü yeguma ga nüma rü marü taguma i natchi rü nüma ga naãpüǖ rü marü manarütchaapüǖ,"""
