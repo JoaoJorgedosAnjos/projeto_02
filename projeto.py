5edf62c37b2846dc095183a980e1135410eb9b1a
joaojorge <joaojorgedosanjos@gmail.com>
2024-09-09 13:05:17 -0300

    :recycle: refactor: alteração de nome de variável

--- projeto.py
+++ projeto.py
@@ -10,25 +10,25 @@ def construir_dicionario_traducao(conto_portugues,conto_ticuna):
             dicionario_portugues_ticuna[i-1] = (conto_portugues[i][3:],conto_ticuna[i][3:])
     return dicionario_portugues_ticuna    
     
-def combinar_textos_recursivamente(tamanho,dicionario,tamanho_inicial_dic):
-    if tamanho == 0:
+def combinar_textos_recursivamente(tamanho_dic,dicionario,tamanho_inicial_dic):
+    if tamanho_dic == 0:
         print('[')
-    elif tamanho < tamanho_inicial_dic:        
-        combinar_textos_recursivamente(tamanho -1, dicionario,tamanho_inicial_dic)
+    elif tamanho_dic < tamanho_inicial_dic:        
+        combinar_textos_recursivamente(tamanho_dic -1, dicionario,tamanho_inicial_dic)
         print(' {')
-        print(f'  "id": {tamanho-1},')
+        print(f'  "id": {tamanho_dic-1},')
         print(f'  "translation": {{')
-        print(f'   "pt": "{dicionario[tamanho-1][0]}",')
-        print(f'   "tca": "{dicionario[tamanho-1][1]}"')
+        print(f'   "pt": "{dicionario[tamanho_dic-1][0]}",')
+        print(f'   "tca": "{dicionario[tamanho_dic-1][1]}"')
         print('  }')
         print(' },')
     else:
-        combinar_textos_recursivamente(tamanho -1, dicionario,tamanho_inicial_dic)
+        combinar_textos_recursivamente(tamanho_dic-1, dicionario,tamanho_inicial_dic)
         print(' {')
-        print(f'  "id": {tamanho-1},')
+        print(f'  "id": {tamanho_dic-1},')
         print(f'  "translation": {{')
-        print(f'   "pt": "{dicionario[tamanho-1][0]}",')
-        print(f'   "tca": "{dicionario[tamanho-1][1]}"')
+        print(f'   "pt": "{dicionario[tamanho_dic-1][0]}",')
+        print(f'   "tca": "{dicionario[tamanho_dic-1][1]}"')
         print('  }')
         print(' }')
         print(']')
